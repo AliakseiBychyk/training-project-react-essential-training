0b23b85fb389f85bfaa0c07261d6de7ab686d9dd
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-05-05 17:29:35 +0300

    several versions of SkiDayCount

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayList } from './components/SkiDayList'
+// import { SkiDayList } from './components/SkiDayList'
+// import { SkiDayCount } from './components/SkiDayCount'
+import {SkiDayCount} from './components/SkiDayCount-createClass'
 
 window.React = React
 
 render(
-  <SkiDayList days={[
-    {
-      resort: "Squaw Valley",
-      date: new Date("1/2/2017"),
-      powder: true,
-      backcountry: false
-    },
-    {
-      resort: "Kirkwood",
-      date: new Date("3/21/2017"),
-      powder: false,
-      backcountry: false
-    },
-    {
-      resort: "Mt. Tallac",
-      date: new Date("4/8/2017"),
-      powder: false,
-      backcountry: true
-    },
-  ]}/>,
+  <SkiDayCount />,
   document.getElementById('react-container')
 )
+
+// render(
+//   <SkiDayList days={[
+//     {
+//       resort: "Squaw Valley",
+//       date: new Date("1/2/2017"),
+//       powder: true,
+//       backcountry: false
+//     },
+//     {
+//       resort: "Kirkwood",
+//       date: new Date("3/21/2017"),
+//       powder: false,
+//       backcountry: false
+//     },
+//     {
+//       resort: "Mt. Tallac",
+//       date: new Date("4/8/2017"),
+//       powder: false,
+//       backcountry: true
+//     },
+//   ]}/>,
+//   document.getElementById('react-container')
+// )

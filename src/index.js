454dc04a7d8592be0aeefd09e49602cf89f8cfdb
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-05-05 17:07:43 +0300

    composing two components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render(
-  <SkiDayCount  total={50}
-                powder={20}
-                backcountry={10}
-                goal={100}/>,
+  <SkiDayList days={[
+    {
+      resort: "Squaw Valley",
+      date: new Date("1/2/2017"),
+      powder: true,
+      backcountry: false
+    },
+    {
+      resort: "Kirkwood",
+      date: new Date("3/21/2017"),
+      powder: false,
+      backcountry: false
+    },
+    {
+      resort: "Mt. Tallac",
+      date: new Date("4/8/2017"),
+      powder: false,
+      backcountry: true
+    },
+  ]}/>,
   document.getElementById('react-container')
 )

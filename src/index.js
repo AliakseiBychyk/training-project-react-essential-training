df6fb7b76d7a6e8111856ebe4669bd0f6f1cef62
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-05-06 11:53:12 +0300

    working with state

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayList } from './components/SkiDayList'
+import { App } from './components/App.js'
+// import { SkiDayList } from './components/SkiDayList'
 // import { SkiDayCount } from './components/SkiDayCount'
 
 window.React = React
@@ -11,25 +12,6 @@ window.React = React
 // )
 
 render(
-  <SkiDayList days={[
-    {
-      resort: "Squaw Valley",
-      date: new Date("1/2/2017"),
-      powder: true,
-      backcountry: false
-    },
-    {
-      resort: "Kirkwood",
-      date: new Date("3/21/2017"),
-      powder: false,
-      backcountry: false
-    },
-    {
-      resort: "Mt. Tallac",
-      date: new Date("4/8/2017"),
-      powder: false,
-      backcountry: true
-    },
-  ]}/>,
+  <App />,
   document.getElementById('react-container')
 )
